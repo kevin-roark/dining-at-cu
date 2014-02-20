5be384d890bbe5f92366e944aacfb876fd353db2
Kevin Roark <ker2143@columbia.edu>
2014-02-20 14:18:55 -0500

    such design

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(function() {
+
+    function blurElement(el, blur) {
+      var filter = 'blur(' + blur + 'px)';
+      var $el = $(el);
+      $el.css('-webkit-filter', filter);
+      $el.css('-moz-filter', filter);
+      $el.css('-o-filter', filter);
+      $el.css('-ms-filter', filter);
+      $el.css('filter', filter);
+    }
+
+    var pizzaBg = $('.splash-background-container');
+
+    $(document).scroll(function() {
+      var scrollTop = $(document).scrollTop();
+      var blurAmt = scrollTop * 0.02;
+      blurElement(pizzaBg, blurAmt);
+
+      if (scrollTop > 400) {
+        $('.menu-date-input').focus();
+      }
+    });
+
+    $('.menu-form').submit(function(ev) {
+
+      var dateStr = $('.menu-date-input').val();
+      var date = chrono.parseDate(dateStr);
+      if (!date) {
+        // handle problem
+      } else {
+        var day = ''  + date.getDate();
+        if (day.length <= 1) {
+          day = '0' + day;
+        }
+        var month = '' + (date.getMonth() + 1);
+        if (month.length <= 1) {
+          month = '0' + month;
+        }
+        var year = ('' + date.getFullYear()).substring(2);
+
+        var formattedDate = month + '/' + day + '/' + year;
+      }
+
+      return false;
+    });
+});
